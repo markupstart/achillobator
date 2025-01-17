477ab95f0db96f120c9e9e9e11823505045aadf9
Mark <mark@upstarters.com>
2025-01-17 15:31:46 -0500

    Update 20-packages.sh

--- build_scripts/20-packages.sh
+++ build_scripts/20-packages.sh
@@ -44,7 +44,10 @@ dnf -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:staging install \
 
 dnf -y --enablerepo "copr:copr.fedorainfracloud.org:ublue-os:staging" install uupd &&
 	dnf -y install systemd-container
-dnf -y --enablerepo "https://copr.fedorainfracloud.org/coprs/yalter/niri-git/repo/epel-${MAJOR_VERSION}/yalter-niri-git-epel-${MAJOR_VERSION}.repo" install niri
+dnf config-manager --add-repo "https://copr.fedorainfracloud.org/coprs/yalter/niri-git/repo/epel-${MAJOR_VERSION}/yalter-niri-git-epel-${MAJOR_VERSION}.repo"
+dnf config-manager --set-disabled copr:copr.fedorainfracloud.org:yalter:niri-git
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:yalter:niri-git install \
+	niri
 dnf -y --enablerepo "copr:copr.fedorainfracloud.org:ublue-os:staging" install ublue-setup-services &&
 	systemctl enable check-sb-key.service
 
